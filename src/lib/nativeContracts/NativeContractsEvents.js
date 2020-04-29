e6b22c6715242a06e077e8b8876cae63076d1f94
emi <emiliorizzo@gmail.com>
2020-04-29 15:10:12 -0600

    Refactor native contracts fake ABI to module

--- src/lib/nativeContracts/NativeContractsEvents.js
+++ src/lib/nativeContracts/NativeContractsEvents.js
@@ -1,9 +1,9 @@
 import { remove0x, add0x, rlp } from 'rsk-utils'
-import * as btcUtils from '../btcUtils'
-import { addSignatureDataToAbi, getSignatureDataFromAbi } from '../utils'
-
+import { getSignatureDataFromAbi } from '../utils'
+import FakeAbi from './FakeABI'
 export function NativeContractsEvents ({ bitcoinNetwork } = {}) {
   const network = bitcoinNetwork || 'testnet'
+  const fakeAbi = FakeAbi(network)
   const decodeAddress = address => {
     address = Buffer.from(remove0x(address), 'hex')
     return add0x(address.toString('hex').slice(-40))
@@ -23,136 +23,6 @@ export function NativeContractsEvents ({ bitcoinNetwork } = {}) {
     if (!Array.isArray(decoded)) decoded = [decoded]
     return decoded.map(d => add0x(removeEmptyStartBytes(d).toString('hex')))
   }
-
-  const decodeBtcTxHash = data => {
-    if (remove0x(data).length === 128) {
-      let buffer = Buffer.from(remove0x(data), 'hex')
-      data = add0x(buffer.toString('ascii'))
-    }
-    return data
-  }
-  const decodeArray = data => data.map(d => Array.isArray(d) ? decodeArray(d) : add0x(d.toString('hex')))
-
-  const decodeFederationData = data => {
-    let [a160, keys] = data
-    let address = btcUtils.h160toAddress(a160, { prefixKey: 'scriptHash', network }).toString('hex')
-    keys = keys.map(d => btcUtils.rskAddressFromBtcPublicKey(d.toString('hex')))
-    return [address, keys]
-  }
-
-  const commitFederationDecoder = data => {
-    const decoded = rlp.decode(data)
-    let [oldData, newData, block] = decoded
-    let [oldFederationAddress, oldFederationMembers] = decodeFederationData(oldData)
-    let [newFederationAddress, newFederationMembers] = decodeFederationData(newData)
-    block = block.toString('ascii')
-    return [oldFederationAddress, oldFederationMembers, newFederationAddress, newFederationMembers, block]
-  }
-  const fakeAbi = Object.freeze(addSignatureDataToAbi([
-    { // Remasc events
-      anonymous: false,
-      inputs: [
-        {
-          indexed: true,
-          name: 'to',
-          type: 'address'
-        },
-        {
-          indexed: false,
-          name: 'blockHash',
-          type: 'string'
-        },
-        {
-          indexed: false,
-          name: 'value',
-          type: 'uint256'
-        }
-      ],
-      name: 'mining_fee_topic',
-      type: 'event'
-    },
-    { // Bridge events
-      inputs: [
-        {
-          indexed: false,
-          name: 'btcTxHash',
-          type: 'string'
-        },
-        {
-          indexed: false,
-          name: 'btcTx', // raw tx?
-          type: 'string'
-        }
-      ],
-      name: 'release_btc_topic',
-      type: 'event'
-    },
-    {
-      inputs: [
-        {
-          indexed: false,
-          name: 'sender',
-          type: 'address'
-        }
-      ],
-      name: 'update_collections_topic',
-      type: 'event'
-    },
-    {
-      inputs: [
-        {
-          indexed: false,
-          name: 'btcTxHash',
-          type: 'string',
-          _filter: decodeBtcTxHash
-        },
-        {
-          indexed: false,
-          name: 'federatorPublicKey',
-          type: 'string'
-        },
-        {
-          indexed: false,
-          name: 'rskTxHash',
-          type: 'string'
-        }
-      ],
-      name: 'add_signature_topic',
-      type: 'event'
-    },
-    {
-      inputs: [
-        {
-          indexed: false,
-          name: 'oldFederationAddress',
-          type: 'string'
-        },
-        {
-          indexed: false,
-          name: 'oldFederationMembers',
-          type: 'address[]'
-        },
-        {
-          indexed: false,
-          name: 'newFederationAddress',
-          type: 'string'
-        },
-        {
-          indexed: false,
-          name: 'newFederationMembers',
-          type: 'address[]'
-        },
-        {
-          indexed: false,
-          name: 'activationBlockNumber',
-          type: 'string'
-        }
-      ],
-      name: 'commit_federation_topic',
-      type: 'event',
-      _decoder: commitFederationDecoder
-    }
-  ]))
 
   const getEventAbi = eventName => fakeAbi.find(a => a.name === eventName && a.type === 'event')
 
