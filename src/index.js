7adf2d763d541c875fed65ec267d8225a6163804
emi <emiliorizzo@gmail.com>
2020-11-18 11:07:41 -0700

    Update exports, add Contract and abi

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 import { ContractParser } from './lib/ContractParser'
 import { BcSearch } from './lib/BcSearch'
-export { ContractParser, BcSearch }
+import Contract from './lib/Contract'
+import bridge from './lib/nativeContracts/bridgeAbi'
+const abi = { bridge }
+export { ContractParser, BcSearch, Contract, abi }
 export default ContractParser
