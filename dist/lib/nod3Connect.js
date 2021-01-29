e2ff5d472ac7b0692fb5c7a74eb7102575a07d4c
emi <emiliorizzo@gmail.com>
2021-01-28 17:15:38 -0700

    Build 0.0.9

--- dist/lib/nod3Connect.js
+++ dist/lib/nod3Connect.js
@@ -1,7 +1,7 @@
-"use strict";Object.defineProperty(exports, "__esModule", { value: true });exports.default = exports.nod3Connect = void 0;var _nod = _interopRequireDefault(require("nod3"));function _interopRequireDefault(obj) {return obj && obj.__esModule ? obj : { default: obj };}
+"use strict";Object.defineProperty(exports, "__esModule", { value: true });exports.default = exports.nod3Connect = void 0;var _nod = _interopRequireDefault(require("@rsksmart/nod3"));function _interopRequireDefault(obj) {return obj && obj.__esModule ? obj : { default: obj };}
 
 const nod3Connect = url => {
-  url = url || 'http://localhost:4444';
+  url = url || process.env['RSK_NODE_URL'] || 'http://localhost:4444';
   return new _nod.default(
   new _nod.default.providers.HttpProvider(url));
 
