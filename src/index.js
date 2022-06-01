46152e53292f90066cd9fbaaad32af6253c190d6
Ubuntu <kayseriiovlabs>
2022-06-01 02:33:17 +0000

    exporting constants

--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ import { BcSearch } from './lib/BcSearch'
 import Contract from './lib/Contract'
 import bridge from './lib/nativeContracts/bridgeAbi'
 import { contractsInterfaces } from './lib/types'
+import * as Constants from './lib/Constants';
 const abi = { bridge }
 const types = { contractsInterfaces }
-export { ContractParser, BcSearch, Contract, abi, types }
+export { ContractParser, BcSearch, Contract, abi, types, Constants }
 export default ContractParser
