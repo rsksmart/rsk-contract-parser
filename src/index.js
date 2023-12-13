0e966034220cce725ada29c615b4828c57b5bb25
Jesus Steer <jsteerv@gmail.com>
2023-12-13 17:16:33 -0500

    fix: removed duplicate definition from logs

--- src/index.js
+++ src/index.js
@@ -5,3 +5,11 @@ import { getBridgeAbi } from './lib/nativeContracts/bridgeAbi'
 const abi = { bridge: getBridgeAbi }
 export { ContractParser, BcSearch, Contract, abi }
 export default ContractParser
+
+// HOTFIX: because the dependency @ethersproject/abi:5.7.0 spam the logs with 'duplicate definition' warning, this temporary fix should silence those messages until a newer version of the library fixes the issue
+const originalConsoleLog = console.log
+console.log = function (message) {
+  if (!message.includes('duplicate definition')) {
+    originalConsoleLog(message)
+  }
+}
