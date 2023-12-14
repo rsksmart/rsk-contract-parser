0a4dda8a8b43263dfb6ccc8608c9837a93d0f7cc
Jesus Steer <jsteerv@gmail.com>
2023-12-14 10:28:15 -0500

    fix: removed duplicate definition from logs

--- dist/lib/nativeContracts/NativeContracts.js
+++ dist/lib/nativeContracts/NativeContracts.js
@@ -1,12 +1,12 @@
-"use strict";Object.defineProperty(exports, "__esModule", { value: true });exports.NativeContracts = NativeContracts;exports.default = exports.parseNativeContracts = exports.defaultNativeContracts = void 0;
+"use strict";Object.defineProperty(exports, "__esModule", { value: true });exports.NativeContracts = NativeContracts;exports.parseNativeContracts = exports.defaultNativeContracts = exports.default = void 0;
 var _rskUtils = require("@rsksmart/rsk-utils");
 
-const defaultNativeContracts = {
+const defaultNativeContracts = exports.defaultNativeContracts = {
   bridge: '0x0000000000000000000000000000000001000006',
-  remasc: '0x0000000000000000000000000000000001000008' };exports.defaultNativeContracts = defaultNativeContracts;
+  remasc: '0x0000000000000000000000000000000001000008'
+};
 
-
-const parseNativeContracts = nativeContracts => {
+const parseNativeContracts = (nativeContracts) => {
   if (typeof nativeContracts !== 'object') throw new TypeError(`nativeContracts must be an object`);
   if (Object.keys(nativeContracts) < 1) throw new Error(`Empty native contracts list`);
   for (let name in nativeContracts) {
@@ -21,19 +21,20 @@ function NativeContracts({ nativeContracts } = {}) {
   nativeContracts = parseNativeContracts(nativeContracts || defaultNativeContracts);
   const names = Object.keys(nativeContracts);
 
-  const getNativeContractAddress = contractName => {
+  const getNativeContractAddress = (contractName) => {
     return nativeContracts[contractName];
   };
-  const getNativeContractName = address => {
+  const getNativeContractName = (address) => {
     address = address.toLowerCase();
-    return names.find(name => nativeContracts[name] === address);
+    return names.find((name) => nativeContracts[name] === address);
   };
 
-  const isNativeContract = address => !!getNativeContractName(address);
+  const isNativeContract = (address) => !!getNativeContractName(address);
 
   const list = () => nativeContracts;
 
   return Object.freeze({ getNativeContractAddress, getNativeContractName, isNativeContract, list });
-}var _default =
+}var _default = exports.default =
 
-NativeContracts;exports.default = _default;
+NativeContracts;
+//# sourceMappingURL=NativeContracts.js.map
