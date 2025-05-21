a6c7b91aa0ecec79539d351a5447fc5c47ea2464
Nicolas Vargas <nicolas.vargas@iovlabs.org>
2025-05-21 20:29:17 -0300

    tests: update tests. Add parseTxLogs tests coverage

--- test/TestContracts/Bridge/Bridge.js
+++ test/TestContracts/Bridge/Bridge.js
@@ -1,5 +1,92 @@
 import { getLatestBridgeAbi, getBridgeAddress, getLatestBridgeMethods } from '../../../src/lib/utils'
 
+const updateCollectionsEvent = {
+  log: {
+    logIndex: 0,
+    blockNumber: 5650461,
+    blockHash: '0x6ddec160925fb958111ae90a21c9644224c0fcd9ea6f140601524c9414f3146a',
+    transactionHash: '0x6592b9f355ebb610673e9fac6e431e78dda457d22dac83ebf4dc77e805c563e7',
+    transactionIndex: 2,
+    address: '0x0000000000000000000000000000000001000006',
+    data: '0x00000000000000000000000047442b55635c9888a2790b598935eb7aac2603e2',
+    topics: [
+      '0x1069152f4f916cbf155ee32a695d92258481944edb5b6fd649718fc1b43e515e'
+    ]
+  },
+  expectedEvent: {
+    withUnverifiedAbi: {
+      logIndex: 0,
+      blockNumber: 5650461,
+      blockHash: '0x6ddec160925fb958111ae90a21c9644224c0fcd9ea6f140601524c9414f3146a',
+      transactionHash: '0x6592b9f355ebb610673e9fac6e431e78dda457d22dac83ebf4dc77e805c563e7',
+      transactionIndex: 2,
+      address: '0x0000000000000000000000000000000001000006',
+      data: '0x00000000000000000000000047442b55635c9888a2790b598935eb7aac2603e2',
+      topics: [
+        '0x1069152f4f916cbf155ee32a695d92258481944edb5b6fd649718fc1b43e515e'
+      ],
+      signature: '1069152f4f916cbf155ee32a695d92258481944edb5b6fd649718fc1b43e515e',
+      event: 'update_collections',
+      args: [
+        '0x47442b55635c9888a2790b598935eb7aac2603e2'
+      ],
+      abi: {
+        type: 'event',
+        anonymous: false,
+        name: 'update_collections',
+        inputs: [
+          {
+            type: 'address',
+            name: 'sender',
+            indexed: false
+          }
+        ]
+      },
+      _addresses: [
+        '0x47442b55635c9888a2790b598935eb7aac2603e2'
+      ],
+      eventId: '056381d00200040601524c9414f3146a',
+      timestamp: 1695046701,
+      txStatus: '0x1'
+    },
+    withVerifiedAbi: {
+      logIndex: 0,
+      blockNumber: 5650461,
+      blockHash: '0x6ddec160925fb958111ae90a21c9644224c0fcd9ea6f140601524c9414f3146a',
+      transactionHash: '0x6592b9f355ebb610673e9fac6e431e78dda457d22dac83ebf4dc77e805c563e7',
+      transactionIndex: 2,
+      address: '0x0000000000000000000000000000000001000006',
+      data: '0x00000000000000000000000047442b55635c9888a2790b598935eb7aac2603e2',
+      topics: [
+        '0x1069152f4f916cbf155ee32a695d92258481944edb5b6fd649718fc1b43e515e'
+      ],
+      signature: '1069152f4f916cbf155ee32a695d92258481944edb5b6fd649718fc1b43e515e',
+      event: 'update_collections',
+      args: [
+        '0x47442b55635c9888a2790b598935eb7aac2603e2'
+      ],
+      abi: {
+        type: 'event',
+        anonymous: false,
+        name: 'update_collections',
+        inputs: [
+          {
+            type: 'address',
+            name: 'sender',
+            indexed: false
+          }
+        ]
+      },
+      _addresses: [
+        '0x47442b55635c9888a2790b598935eb7aac2603e2'
+      ],
+      eventId: '056381d00200040601524c9414f3146a',
+      timestamp: 1695046701,
+      txStatus: '0x1'
+    }
+  }
+}
+
 export const Bridge = {
   abi: getLatestBridgeAbi(),
   bytecode: '0x',
@@ -19,5 +106,12 @@ export const Bridge = {
     unverifiedImplementationInterfaces: null,
     verifiedImplementationMethods: null,
     verifiedImplementationInterfaces: null
-  }
+  },
+  events: [
+    /*
+      Bridge: ABI is always known. The only case where the parser is unable to decode a bridge event is when the parser is not using the proper height-matched bridge ABI. (eg: new RSK nodes release)
+      Example: A "TestEvent" event is emmited at block 100, but the parser is using an older bridge ABI valid from block 1 to 99, which doesn't support the "TestEvent" event. This will cause the parser to be unable to decode logs related to that event.
+    */
+    updateCollectionsEvent
+  ]
 }
--- test/TestContracts/USDCe/USDCe.js
+++ test/TestContracts/USDCe/USDCe.js
@@ -3,6 +3,414 @@ import USDCe_BYTECODE from './USDCe_bytecode.js'
 import USDCe_impl_ABI from './USDCe_impl_ABI.json'
 import USDCe_impl_BYTECODE from './USDCe_impl_bytecode.js'
 
+const TransferEvent1 = {
+  log: {
+    logIndex: 0,
+    blockNumber: 7385540,
+    blockHash: '0x209907180e7f474c19891506d91e26929618be9576e829e70c73f82621b50169',
+    transactionHash: '0xa8eb5a03859a82ce55277993d781312cfca0bfd8b24f8d8f2e8e050c3299b6d6',
+    transactionIndex: 2,
+    address: '0x74c9f2b00581f1b11aa7ff05aa9f608b7389de67',
+    data: '0x0000000000000000000000000000000000000000000000000000000002831c6b',
+    topics: [
+      '0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef',
+      '0x0000000000000000000000008fb68e5960bfb9c6e1ae6420b767ccd0287b514c',
+      '0x000000000000000000000000af54be5b6eec24d6bfacf1cce4eaf680a8239398'
+    ]
+  },
+  expectedEvent: {
+    withUnverifiedAbi: {
+      logIndex: 0,
+      blockNumber: 7385540,
+      blockHash: '0x209907180e7f474c19891506d91e26929618be9576e829e70c73f82621b50169',
+      transactionHash: '0xa8eb5a03859a82ce55277993d781312cfca0bfd8b24f8d8f2e8e050c3299b6d6',
+      transactionIndex: 2,
+      address: '0x74c9f2b00581f1b11aa7ff05aa9f608b7389de67',
+      data: '0x0000000000000000000000000000000000000000000000000000000002831c6b',
+      topics: [
+        '0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef',
+        '0x0000000000000000000000008fb68e5960bfb9c6e1ae6420b767ccd0287b514c',
+        '0x000000000000000000000000af54be5b6eec24d6bfacf1cce4eaf680a8239398'
+      ],
+      signature: 'ddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef',
+      event: 'Transfer',
+      args: [
+        '0x8fb68e5960bfb9c6e1ae6420b767ccd0287b514c',
+        '0xaf54be5b6eec24d6bfacf1cce4eaf680a8239398',
+        '0x02831c6b'
+      ],
+      abi: {
+        type: 'event',
+        anonymous: false,
+        name: 'Transfer',
+        inputs: [
+          {
+            type: 'address',
+            name: 'from',
+            indexed: true
+          },
+          {
+            type: 'address',
+            name: 'to',
+            indexed: true
+          },
+          {
+            type: 'uint256',
+            name: 'value',
+            indexed: false
+          }
+        ]
+      },
+      _addresses: [
+        '0x8fb68e5960bfb9c6e1ae6420b767ccd0287b514c',
+        '0xaf54be5b6eec24d6bfacf1cce4eaf680a8239398'
+      ],
+      eventId: '070b1c40020009e70c73f82621b50169',
+      timestamp: 1743069058,
+      txStatus: '0x1'
+    },
+    withVerifiedAbi: {
+      logIndex: 0,
+      blockNumber: 7385540,
+      blockHash: '0x209907180e7f474c19891506d91e26929618be9576e829e70c73f82621b50169',
+      transactionHash: '0xa8eb5a03859a82ce55277993d781312cfca0bfd8b24f8d8f2e8e050c3299b6d6',
+      transactionIndex: 2,
+      address: '0x74c9f2b00581f1b11aa7ff05aa9f608b7389de67',
+      data: '0x0000000000000000000000000000000000000000000000000000000002831c6b',
+      topics: [
+        '0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef',
+        '0x0000000000000000000000008fb68e5960bfb9c6e1ae6420b767ccd0287b514c',
+        '0x000000000000000000000000af54be5b6eec24d6bfacf1cce4eaf680a8239398'
+      ],
+      signature: 'ddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef',
+      event: 'Transfer',
+      args: [
+        '0x8fb68e5960bfb9c6e1ae6420b767ccd0287b514c',
+        '0xaf54be5b6eec24d6bfacf1cce4eaf680a8239398',
+        '0x02831c6b'
+      ],
+      abi: {
+        type: 'event',
+        anonymous: false,
+        name: 'Transfer',
+        inputs: [
+          {
+            type: 'address',
+            name: 'from',
+            indexed: true
+          },
+          {
+            type: 'address',
+            name: 'to',
+            indexed: true
+          },
+          {
+            type: 'uint256',
+            name: 'value',
+            indexed: false
+          }
+        ]
+      },
+      _addresses: [
+        '0x8fb68e5960bfb9c6e1ae6420b767ccd0287b514c',
+        '0xaf54be5b6eec24d6bfacf1cce4eaf680a8239398'
+      ],
+      eventId: '070b1c40020009e70c73f82621b50169',
+      timestamp: 1743069058,
+      txStatus: '0x1'
+    }
+  }
+}
+
+const BurnEvent = {
+  log: {
+    logIndex: 1,
+    blockNumber: 7385540,
+    blockHash: '0x209907180e7f474c19891506d91e26929618be9576e829e70c73f82621b50169',
+    transactionHash: '0xa8eb5a03859a82ce55277993d781312cfca0bfd8b24f8d8f2e8e050c3299b6d6',
+    transactionIndex: 2,
+    address: '0x74c9f2b00581f1b11aa7ff05aa9f608b7389de67',
+    data: '0x0000000000000000000000000000000000000000000000000000000002831c6b',
+    topics: [
+      '0xcc16f5dbb4873280815c1ee09dbd06736cffcc184412cf7a71a0fdb75d397ca5',
+      '0x000000000000000000000000af54be5b6eec24d6bfacf1cce4eaf680a8239398'
+    ],
+    abi: {},
+    eventId: '070b1c40020019e70c73f82621b50169',
+    timestamp: 1743069058,
+    txStatus: '0x1',
+    event: null,
+    _addresses: []
+  },
+  expectedEvent: {
+    withUnverifiedAbi: {
+      logIndex: 1,
+      blockNumber: 7385540,
+      blockHash: '0x209907180e7f474c19891506d91e26929618be9576e829e70c73f82621b50169',
+      transactionHash: '0xa8eb5a03859a82ce55277993d781312cfca0bfd8b24f8d8f2e8e050c3299b6d6',
+      transactionIndex: 2,
+      address: '0x74c9f2b00581f1b11aa7ff05aa9f608b7389de67',
+      data: '0x0000000000000000000000000000000000000000000000000000000002831c6b',
+      topics: [
+        '0xcc16f5dbb4873280815c1ee09dbd06736cffcc184412cf7a71a0fdb75d397ca5',
+        '0x000000000000000000000000af54be5b6eec24d6bfacf1cce4eaf680a8239398'
+      ],
+      abi: {},
+      eventId: '070b1c40020019e70c73f82621b50169',
+      timestamp: 1743069058,
+      txStatus: '0x1',
+      event: null,
+      _addresses: []
+    },
+    withVerifiedAbi: {
+      logIndex: 1,
+      blockNumber: 7385540,
+      blockHash: '0x209907180e7f474c19891506d91e26929618be9576e829e70c73f82621b50169',
+      transactionHash: '0xa8eb5a03859a82ce55277993d781312cfca0bfd8b24f8d8f2e8e050c3299b6d6',
+      transactionIndex: 2,
+      address: '0x74c9f2b00581f1b11aa7ff05aa9f608b7389de67',
+      data: '0x0000000000000000000000000000000000000000000000000000000002831c6b',
+      topics: [
+        '0xcc16f5dbb4873280815c1ee09dbd06736cffcc184412cf7a71a0fdb75d397ca5',
+        '0x000000000000000000000000af54be5b6eec24d6bfacf1cce4eaf680a8239398'
+      ],
+      signature: 'cc16f5dbb4873280815c1ee09dbd06736cffcc184412cf7a71a0fdb75d397ca5',
+      event: 'Burn',
+      args: [
+        '0xaf54be5b6eec24d6bfacf1cce4eaf680a8239398',
+        '0x02831c6b'
+      ],
+      abi: {
+        type: 'event',
+        anonymous: false,
+        name: 'Burn',
+        inputs: [
+          {
+            type: 'address',
+            name: 'burner',
+            indexed: true
+          },
+          {
+            type: 'uint256',
+            name: 'amount',
+            indexed: false
+          }
+        ]
+      },
+      _addresses: [
+        '0xaf54be5b6eec24d6bfacf1cce4eaf680a8239398'
+      ],
+      eventId: '070b1c40020019e70c73f82621b50169',
+      timestamp: 1743069058,
+      txStatus: '0x1'
+    }
+  }
+}
+
+const TransferEvent2 = {
+  log: {
+    logIndex: 2,
+    blockNumber: 7385540,
+    blockHash: '0x209907180e7f474c19891506d91e26929618be9576e829e70c73f82621b50169',
+    transactionHash: '0xa8eb5a03859a82ce55277993d781312cfca0bfd8b24f8d8f2e8e050c3299b6d6',
+    transactionIndex: 2,
+    address: '0x74c9f2b00581f1b11aa7ff05aa9f608b7389de67',
+    data: '0x0000000000000000000000000000000000000000000000000000000002831c6b',
+    topics: [
+      '0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef',
+      '0x000000000000000000000000af54be5b6eec24d6bfacf1cce4eaf680a8239398',
+      '0x0000000000000000000000000000000000000000000000000000000000000000'
+    ]
+  },
+  expectedEvent: {
+    withUnverifiedAbi: {
+      logIndex: 2,
+      blockNumber: 7385540,
+      blockHash: '0x209907180e7f474c19891506d91e26929618be9576e829e70c73f82621b50169',
+      transactionHash: '0xa8eb5a03859a82ce55277993d781312cfca0bfd8b24f8d8f2e8e050c3299b6d6',
+      transactionIndex: 2,
+      address: '0x74c9f2b00581f1b11aa7ff05aa9f608b7389de67',
+      data: '0x0000000000000000000000000000000000000000000000000000000002831c6b',
+      topics: [
+        '0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef',
+        '0x000000000000000000000000af54be5b6eec24d6bfacf1cce4eaf680a8239398',
+        '0x0000000000000000000000000000000000000000000000000000000000000000'
+      ],
+      signature: 'ddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef',
+      event: 'Transfer',
+      args: [
+        '0xaf54be5b6eec24d6bfacf1cce4eaf680a8239398',
+        '0x0000000000000000000000000000000000000000',
+        '0x02831c6b'
+      ],
+      abi: {
+        type: 'event',
+        anonymous: false,
+        name: 'Transfer',
+        inputs: [
+          {
+            type: 'address',
+            name: 'from',
+            indexed: true
+          },
+          {
+            type: 'address',
+            name: 'to',
+            indexed: true
+          },
+          {
+            type: 'uint256',
+            name: 'value',
+            indexed: false
+          }
+        ]
+      },
+      _addresses: [
+        '0xaf54be5b6eec24d6bfacf1cce4eaf680a8239398',
+        '0x0000000000000000000000000000000000000000'
+      ],
+      eventId: '070b1c40020029e70c73f82621b50169',
+      timestamp: 1743069058,
+      txStatus: '0x1'
+    },
+    withVerifiedAbi: {
+      logIndex: 2,
+      blockNumber: 7385540,
+      blockHash: '0x209907180e7f474c19891506d91e26929618be9576e829e70c73f82621b50169',
+      transactionHash: '0xa8eb5a03859a82ce55277993d781312cfca0bfd8b24f8d8f2e8e050c3299b6d6',
+      transactionIndex: 2,
+      address: '0x74c9f2b00581f1b11aa7ff05aa9f608b7389de67',
+      data: '0x0000000000000000000000000000000000000000000000000000000002831c6b',
+      topics: [
+        '0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef',
+        '0x000000000000000000000000af54be5b6eec24d6bfacf1cce4eaf680a8239398',
+        '0x0000000000000000000000000000000000000000000000000000000000000000'
+      ],
+      signature: 'ddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef',
+      event: 'Transfer',
+      args: [
+        '0xaf54be5b6eec24d6bfacf1cce4eaf680a8239398',
+        '0x0000000000000000000000000000000000000000',
+        '0x02831c6b'
+      ],
+      abi: {
+        type: 'event',
+        anonymous: false,
+        name: 'Transfer',
+        inputs: [
+          {
+            type: 'address',
+            name: 'from',
+            indexed: true
+          },
+          {
+            type: 'address',
+            name: 'to',
+            indexed: true
+          },
+          {
+            type: 'uint256',
+            name: 'value',
+            indexed: false
+          }
+        ]
+      },
+      _addresses: [
+        '0xaf54be5b6eec24d6bfacf1cce4eaf680a8239398',
+        '0x0000000000000000000000000000000000000000'
+      ],
+      eventId: '070b1c40020029e70c73f82621b50169',
+      timestamp: 1743069058,
+      txStatus: '0x1'
+    }
+  }
+}
+
+const MinterConfiguredEvent = {
+  log: {
+    logIndex: 0,
+    blockNumber: 7029970,
+    blockHash: '0x8cf326dc92135317f59be79af463f5e7ee7451d4a9b4de9548c5214e459e29d9',
+    transactionHash: '0x55d21cfca76a0161d3721fe74c3ffcdc019b89a3b90db1a829d8378565cf1668',
+    transactionIndex: 13,
+    address: '0x74c9f2b00581f1b11aa7ff05aa9f608b7389de67',
+    data: '0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff',
+    topics: [
+      '0x46980fca912ef9bcdbd36877427b6b90e860769f604e89c0e67720cece530d20',
+      '0x000000000000000000000000af54be5b6eec24d6bfacf1cce4eaf680a8239398'
+    ],
+    abi: {},
+    eventId: '06b44d200d000e9548c5214e459e29d9',
+    timestamp: 1734476754,
+    txStatus: '0x1',
+    event: null,
+    _addresses: []
+  },
+  expectedEvent: {
+    withUnverifiedAbi: {
+      logIndex: 0,
+      blockNumber: 7029970,
+      blockHash: '0x8cf326dc92135317f59be79af463f5e7ee7451d4a9b4de9548c5214e459e29d9',
+      transactionHash: '0x55d21cfca76a0161d3721fe74c3ffcdc019b89a3b90db1a829d8378565cf1668',
+      transactionIndex: 13,
+      address: '0x74c9f2b00581f1b11aa7ff05aa9f608b7389de67',
+      data: '0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff',
+      topics: [
+        '0x46980fca912ef9bcdbd36877427b6b90e860769f604e89c0e67720cece530d20',
+        '0x000000000000000000000000af54be5b6eec24d6bfacf1cce4eaf680a8239398'
+      ],
+      abi: {},
+      eventId: '06b44d200d000e9548c5214e459e29d9',
+      timestamp: 1734476754,
+      txStatus: '0x1',
+      event: null,
+      _addresses: []
+    },
+    withVerifiedAbi: {
+      logIndex: 0,
+      blockNumber: 7029970,
+      blockHash: '0x8cf326dc92135317f59be79af463f5e7ee7451d4a9b4de9548c5214e459e29d9',
+      transactionHash: '0x55d21cfca76a0161d3721fe74c3ffcdc019b89a3b90db1a829d8378565cf1668',
+      transactionIndex: 13,
+      address: '0x74c9f2b00581f1b11aa7ff05aa9f608b7389de67',
+      data: '0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff',
+      topics: [
+        '0x46980fca912ef9bcdbd36877427b6b90e860769f604e89c0e67720cece530d20',
+        '0x000000000000000000000000af54be5b6eec24d6bfacf1cce4eaf680a8239398'
+      ],
+      signature: '46980fca912ef9bcdbd36877427b6b90e860769f604e89c0e67720cece530d20',
+      event: 'MinterConfigured',
+      args: [
+        '0xaf54be5b6eec24d6bfacf1cce4eaf680a8239398',
+        '0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff'
+      ],
+      abi: {
+        type: 'event',
+        anonymous: false,
+        name: 'MinterConfigured',
+        inputs: [
+          {
+            type: 'address',
+            name: 'minter',
+            indexed: true
+          },
+          {
+            type: 'uint256',
+            name: 'minterAllowedAmount',
+            indexed: false
+          }
+        ]
+      },
+      _addresses: [
+        '0xaf54be5b6eec24d6bfacf1cce4eaf680a8239398'
+      ],
+      eventId: '06b44d200d000e9548c5214e459e29d9',
+      timestamp: 1734476754,
+      txStatus: '0x1'
+    }
+  }
+}
+
 export const USDCe = {
   abi: USDCe_ABI,
   bytecode: USDCe_BYTECODE,
@@ -102,5 +510,11 @@ export const USDCe = {
       'version()'
     ],
     verifiedImplementationInterfaces: ['ERC20']
-  }
+  },
+  events: [
+    TransferEvent1,
+    BurnEvent,
+    TransferEvent2,
+    MinterConfiguredEvent
+  ]
 }
