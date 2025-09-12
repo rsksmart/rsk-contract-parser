b62d74572b2baff47b9329cd02f65f1cfe2bc355
Nicolas Vargas <nicolas.vargas@iovlabs.org>
2025-09-12 11:08:44 -0300

    feat: support REED bridge abi and update rsk-precompiled-abis dependency

--- test/bridgeAbi.spec.js
+++ test/bridgeAbi.spec.js
@@ -5,6 +5,7 @@ import iris from '../src/lib/nativeContracts/bridge-iris.json'
 import fingerroot from '../src/lib/nativeContracts/bridge-fingerroot.json'
 import hop from '../src/lib/nativeContracts/bridge-hop.json'
 import lovell from '../src/lib/nativeContracts/bridge-lovell.json'
+import reed from '../src/lib/nativeContracts/bridge-reed.json'
 import { getRskReleaseByBlockNumber, RSK_RELEASES } from '../src/lib/nativeContracts/bridgeAbi'
 
 describe('All abis must be in ascendant order', () => {
@@ -30,13 +31,15 @@ describe('getBridgeAbi(txBlockNumber, bitcoinNetwork) should return the correct 
     { height: 1, abi: orchid, name: 'orchid' },
     { height: 3614801, abi: iris, name: 'iris' },
     { height: 5468005, abi: fingerroot, name: 'fingerroot' },
-    { height: 7338024, abi: lovell, name: 'lovell' }
+    { height: 7338024, abi: lovell, name: 'lovell' },
+    { height: 8052200, abi: reed, name: 'reed' }
   ]
   const testnetTestExpectatins = [
     { height: 0, abi: wasabi, name: 'wasabi' },
     { height: 1, abi: wasabi, name: 'wasabi' },
     { height: 3103001, abi: hop, name: 'hop' },
-    { height: 6110487, abi: lovell, name: 'lovell' }
+    { height: 6110487, abi: lovell, name: 'lovell' },
+    { height: 6835700, abi: reed, name: 'reed' }
   ]
 
   for (const { height, abi, name } of mainnetTestExpectations) {
